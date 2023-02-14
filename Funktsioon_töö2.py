81ea7b50342bbd9be78b1458e6562d6e8fce1798
opilane <opilane@HP-CZC2349HT0>
2023-02-14 14:10:22 +0200

    Add project files.

---
+++ Funktsioon_töö2.py
@@ -0,0 +1,44 @@
+﻿from math import*
+from random import*
+from OmaModul import *
+
+logt=["User1", "User2"]
+pas=["s1mple", "juppi"]
+while True:
+    a=str(input("Вы хотите войти в систему? ")) #yas/no
+    if a=="yas":
+        print("Авторизуйтесь")
+        print("Укажите свой логин и пароль")
+        logi=input("Логин: ")
+        pass_=input("Пароль: ")
+        if logi in logt and pass_ in pas:
+            print("Вы успешно Авторизовались ")
+
+        elif print("Не верный логин или пароль"):
+            print("")
+
+
+    else: 
+        a=="no"
+        b=str(input("Вы хотите регнутся? "))
+        if b=="yas":
+            print("Регистрация")
+            b1=str(input("Автоматическое создание пароля: "))
+            b1=="yas"
+            spin=salasõna
+            print("Пароль:", spin)
+
+            #b1=="no"
+            #if b2=str(input("Cамостоятельное создание пароля")):
+            #print()
+        else:
+            b=="no"
+            c=str(input("Закончить работу? ")) #ainult yas
+            if c=="yas":
+                print("Конец") 
+                break
+
+
+
+
+  
---
+++ OmaModul.py
@@ -0,0 +1,12 @@
+﻿from random import*
+
+def salasõna(sala:int)->any:
+    """
+    Määrme salasõna..
+    :parem int sala:Järjend salasõna numbridest
+    :rtype: any
+    """
+    süm=".,:;!_*-+()/#¤%&0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+    sala=''.join(random.choice(süm) for _ in range(12))
+
+    return sala
