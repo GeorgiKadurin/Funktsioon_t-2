f81be099fa673012822a8e2d03ec6d411aff5054
Georgi Kadurin <120027713+GeorgiKadurin@users.noreply.github.com>
2023-02-16 23:32:36 +0200

    Update OmaModul.py

--- OmaModul.py
+++ OmaModul.py
@@ -1,12 +1,59 @@
-﻿from random import*
+from random import *
+#def salasõna(k:int)->bool:
+#    """
+#    Määrme salasõna..
+#    :parem int sala:Järjend salasõna numbridest
+#    :rtype: bool
+#    """
+#    k='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.,:;!_*-+()/#¤%&'
+#    salasõna=''.join(random.choice(k) for x in range(12)) #Выбирает 12 рандомных символов.
+#    return salasõna
 
-def salasõna(sala:int)->any:
+
+
+import string
+def Salasona(k: int)->bool:
     """
     Määrme salasõna..
-    :parem int sala:Järjend salasõna numbridest
-    :rtype: any
+    :parem int k:Järjend salasõna numbridest
+    :rtype: bool
     """
-    süm=".,:;!_*-+()/#¤%&0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
-    sala=''.join(random.choice(süm) for _ in range(12))
+    saladus=""
+    for i in range(k):
+        t=choice(string.ascii_letters) #Aa...Zz
+        num=choice([1,2,3,4,5,6,7,8,9,0])
+        sym=choice(["*","-",".","!","_"])
+        t_num=[t,str(num),sym]
+        saladus+=choice(t_num)
+    return saladus
 
-    return sala
+def reg(logt:list, pas:str)->bool:
+    """
+    Määrme reg..
+    :parem logt list, pas str:Järjend salasõna numbridest
+    :rtype: bool
+    """
+    n=input("Напишите свое имя: ")
+    tehe=int(input("2-Задать свой пароль, 1-сгенерировать его случайным образом\n "))
+
+    if tehe==1:
+        salasona=Salasona(12)
+        logt.append(n)
+        pas.append(salasona)
+ 
+
+    elif tehe==2:
+       pas=input("Напишите свой пароль: ")
+       if any(c.islower() for c in pas) and any(c.isupper() for c in pas) and any(c.isdigit() for c in pas) and any(c in '.,:;!_*-+()/#¤%&' for c in pas):
+            print("Вы создали пароль")
+            logt.append(n)
+            pas.append(str(salasona))
+       else:
+            print("Ошибка")
+        
+       
+    return logt,pas
+
+
+
+
