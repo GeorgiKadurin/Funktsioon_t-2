d91530cd40cbde6e5603021ade100ee4583a3531
Georgi Kadurin <120027713+GeorgiKadurin@users.noreply.github.com>
2023-02-19 23:16:01 +0200

    Update Funktsioon_töö2.py

--- Funktsioon_töö2.py
+++ Funktsioon_töö2.py
@@ -2,35 +2,47 @@ from OmaModul import *
 
 
 logt=["User1", "User2"]
-pas=["s1mple", "juppi"]
+pasw=["s1mple", "juppi"]
 while True:
     print(logt)
-    print(pas)
+    print(pasw)
     a=str(input("Вы хотите войти в систему? ")) #yas/no
     if a=="yas":
         print("Авторизуйтесь")
         print("Укажите свой логин и пароль")
         logi=input("Логин: ")
         pass_=input("Пароль: ")
-        if logi in logt and pass_ in pas:
+        if logi in logt and pass_ in pasw:
             print("Вы успешно Авторизовались ")
 
-        elif print("Не верный логин или пароль"):
-            print("")
-
+        else:
+            print("Не верный логин или пароль")
+            d=str(input("Вы хотите восстановить пароль? "))
+            if  d=="yas":
+                username = input("Введите имя пользователя: ")
+                old_password = input("Введите старый пароль: ")
+                new_password = input("Введите новый пароль: ")
+                reset_password(logt, pasw, username, old_password, new_password)
+                
+            else:
+                 d=="no"
+                 print()
+                 f=str(input("Вы хотите восстановить забытй пароль? "))
+                 if f=="yas":
+                      username = input("Введите имя пользователя: ")
+                      j=сброс_пароля
 
     elif a=="no":
          b=str(input("Вы хотите регнутся? "))
          b=="yas"
          print("Регистрация")
-         logt,pas=reg(logt,pas)
+         logt,pasw=reg(logt,pasw)
     else:
          b=="no"
          c=str(input("Закончить работу? ")) #ainult yas
          if c=="yas":
             print("Конец") 
             break
-       
 
 
 
