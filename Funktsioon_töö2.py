a98c05326bf83c55ab3606742669bec4162bf04f
Georgi Kadurin <120027713+GeorgiKadurin@users.noreply.github.com>
2023-02-20 12:29:46 +0200

    Update Funktsioon_töö2.py

--- Funktsioon_töö2.py
+++ Funktsioon_töö2.py
@@ -1,49 +1,48 @@
 from OmaModul import *
 
 
-logt=["User1", "User2"]
-pasw=["s1mple", "juppi"]
+Login=["User1", "User2"]
+password_=["s1mple", "juppi"]
 while True:
-    print(logt)
-    print(pasw)
+    print(Login)
+    print(password_)
     a=str(input("Вы хотите войти в систему? ")) #yas/no
     if a=="yas":
         print("Авторизуйтесь")
         print("Укажите свой логин и пароль")
         logi=input("Логин: ")
         pass_=input("Пароль: ")
-        if logi in logt and pass_ in pasw:
+        if logi in Login and pass_ in password_:
             print("Вы успешно Авторизовались ")
 
         else:
             print("Не верный логин или пароль")
             d=str(input("Вы хотите восстановить пароль? "))
             if  d=="yas":
-                username = input("Введите имя пользователя: ")
-                old_password = input("Введите старый пароль: ")
-                new_password = input("Введите новый пароль: ")
-                reset_password(logt, pasw, username, old_password, new_password)
+                print("Восстановление пароля")
+                Logit = input("Введите имя пользователя: ")
+                pas_o = input("Введите старый пароль: ")
+                pas_n = input("Введите новый пароль: ")
+                repass(Login, password_, Logit, pas_o, pas_n)
+                
                 
             else:
                  d=="no"
                  print()
-                 f=str(input("Вы хотите восстановить забытй пароль? "))
+                 f=str(input("Вы хотите восстановить забытый пароль? "))
                  if f=="yas":
-                      username = input("Введите имя пользователя: ")
-                      j=сброс_пароля
+                    Logit= input("Введите имя пользователя: ")
+                    reepasss(password_, Logit, Login)
+                      
 
     elif a=="no":
          b=str(input("Вы хотите регнутся? "))
          b=="yas"
          print("Регистрация")
-         logt,pasw=reg(logt,pasw)
+         Login,password_=reg(Login,password_)
     else:
          b=="no"
          c=str(input("Закончить работу? ")) #ainult yas
          if c=="yas":
             print("Конец") 
             break
-
-
-
-  
