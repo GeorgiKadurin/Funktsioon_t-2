cb19dd2da6d258490fade98ef93adf2cd90462f9
Georgi Kadurin <120027713+GeorgiKadurin@users.noreply.github.com>
2023-02-20 21:59:53 +0200

    Update Funktsioon_töö2.py

--- Funktsioon_töö2.py
+++ Funktsioon_töö2.py
@@ -6,43 +6,53 @@ password_=["s1mple", "juppi"]
 while True:
     print(Login)
     print(password_)
-    a=str(input("Вы хотите войти в систему? ")) #yas/no
+    a=str(input("Kas soovite süsteemi sisse logida? ")) #yas/no
     if a=="yas":
-        print("Авторизуйтесь")
-        print("Укажите свой логин и пароль")
-        logi=input("Логин: ")
-        pass_=input("Пароль: ")
+        print("Autoriseerima")
+        print("Määrake oma kasutajanimi ja parool")
+        logi=input("Login: ")
+        pass_=input("Parool: ")
         if logi in Login and pass_ in password_:
-            print("Вы успешно Авторизовались ")
+            print("Olete Sisse Loginud ")
 
         else:
-            print("Не верный логин или пароль")
-            d=str(input("Вы хотите восстановить пароль? "))
+            print("Vale sisselogimine või parool")
+            d=str(input("Kas soovite parooli taastada? "))
             if  d=="yas":
-                print("Восстановление пароля")
-                Logit = input("Введите имя пользователя: ")
-                pas_o = input("Введите старый пароль: ")
-                pas_n = input("Введите новый пароль: ")
+                print("Parooli taastamine")
+                Logit = input("Sisestage Kasutaja nimi: ")
+                pas_o = input("Sisestage vana parool: ")
+                pas_n = input("Sisestage uus parool: ")
                 repass(Login, password_, Logit, pas_o, pas_n)
+                print()
+            else: 
+                d=="no"
+                k=str(input("Kas soovite taastada sisselogimise? "))
+                if k=="yas":
+                   log_o = input("Sisestage praegune kasutajanimi: ")
+                   log_n = input("Sisestage uus kasutajanimi: ")
+                   reuser(Login, log_o, log_n)
+
                 
-                
-            else:
-                 d=="no"
-                 print()
-                 f=str(input("Вы хотите восстановить забытый пароль? "))
-                 if f=="yas":
-                    Logit= input("Введите имя пользователя: ")
-                    reepasss(password_, Logit, Login)
+                else:
+                    k=="no"
+                    print()
+                    f=str(input("Kas soovite unustatud parooli taastada? "))
+                    if f=="yas":
+                        logit= input("Sisestage oma kasutajanimi: ")
+                        reepasss(password_, logit, Login)
                       
 
     elif a=="no":
-         b=str(input("Вы хотите регнутся? "))
+         b=str(input("Kas soovite registreeruda? "))
          b=="yas"
-         print("Регистрация")
+         print("Registreerimine")
          Login,password_=reg(Login,password_)
+
     else:
+         f=="no"
          b=="no"
-         c=str(input("Закончить работу? ")) #ainult yas
+         c=str(input("Töö lõpetada? ")) #ainult yas
          if c=="yas":
-            print("Конец") 
+            print("Lõpuks") 
             break
